0e24e292dce7df6922e51945e531697aa5342d01
sunil <sunilkumar@globussoft.in>
2020-02-07 15:36:22 +0530

    initial commit2

---
+++ app/src/main/java/com/build/androidbuildtypeandflavors/MainActivity.java
@@ -0,0 +1,15 @@
+package com.build.androidbuildtypeandflavors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
